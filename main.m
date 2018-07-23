75e9237e7c2e0bf20a92be64c73b40846ca5ea66
NestorBonjorn <nestorbonjorn@gmail.com>
2018-07-23 21:44:39 +0200

    Format

--- main.m
+++ main.m
@@ -1,33 +1,25 @@
-% Number of simulations
-num_simulations = 10;
+%% Simulation parameters
+proposal = true; % Flag indicating whether we use our proposed approach or not
+num_simulations = 10; % Number of simulations
+num_subframes_simulated = 1000000;                                  % Number of simulated subframes per simulation
+cumul_cat = zeros(num_subframes_simulated/1000,num_simulations);    % It will store the different simulation results
 
-% Number of simulated subframes per simulation
-num_subframes_simulated = 1000000;
-
-% It will store the different simulation results
-cumul_cat = zeros(num_subframes_simulated/1000,num_simulations);
-
-% Whether we use our proposed approach (proposal=true) or not 
-% (proposal=false)
-proposal = true;
-    
 %% General parameters
-num_vehicles = 10;
-probResourceKeep = 0;
+num_vehicles = 10;      % Number of simulated vehicles
+probResourceKeep = 0.4; % Probability to keep the resources when the counter expires
 
 %% Temporal parameters
-RRI = 20; % Resource Reservation Interval in miliseconds
-T1 = 2; % in miliseconds
-T2 = 20; % in miliseconds
-C1 = 63; % SL_RESOURCE_RESELECTION_COUNTER low boundary
-C2 = 63; % SL_RESOURCE_RESELECTION_COUNTER high boundary
+RRI = 20;   % Resource Reservation Interval in miliseconds
+T1 = 2;     % Resource selection window low boundary in miliseconds
+T2 = 20;    % Resource selection window high boundary in miliseconds
+C1 = 25;    % SL_RESOURCE_RESELECTION_COUNTER low boundary
+C2 = 75;    % SL_RESOURCE_RESELECTION_COUNTER high boundary
 
 %% Frequency parameters
-num_subchannels = 1; % Number of subchannels in the grid
-packet_subchannels = 1; % Number of subchannels that a packet occupies
-                        % (it depends on packet_size, num_subchannels 
-                        % and bandwidth
+N_subch = 1;    % Number of subchannels in the grid
+L_subch = 1;    % Number of subchannels that a packet occupies
     
+% Each iteration of the for loop manages a single simulation
 for i = 1:num_simulations
     
     % Display current simulation number
@@ -36,44 +28,39 @@ for i = 1:num_simulations
     % UEs parameters initialization (random)
     UEs = struct('counter',{},'subframe',{},'subchannels',{});
     for UE = 1:num_vehicles
-        UEs(UE).counter = randi([1,C2]);
-        UEs(UE).subframe = randi([1,RRI]);
-        subchannel_start = randi([1,num_subchannels-(packet_subchannels-1)]);
-        UEs(UE).subchannels = subchannel_start:(subchannel_start+packet_subchannels-1);
+        UEs(UE).counter = randi([1,C2]);                                        % Counter
+        UEs(UE).subframe = randi([1,RRI]);                                      % First subframe where the UE transmits
+        subchannel_start = randi([1,N_subch-(L_subch-1)]);
+        UEs(UE).subchannels = subchannel_start:(subchannel_start+L_subch-1);    % Subchannels where the UE transmits
     end
 
     %% Simulation
-    % Subchannels used in each subframe.
-    sensing = cell(num_subframes_simulated,1); 
-    % Transmitted counter values in each subframe.
-    sensing_counter = cell(num_subframes_simulated,1);
-    % Number of collisions in each subframe.
-    collisions = zeros(num_subframes_simulated,1); 
-    % Number of transmissions in each subframe.
-    num_transmissions_list = zeros(num_subframes_simulated,1); 
     
-    % We iterate for each of the simulated subframes
+    sensing = cell(num_subframes_simulated,1);                  % Subchannels used in each subframe.
+    sensing_counter = cell(num_subframes_simulated,1);          % Transmitted counter values in each subframe.
+    collisions = zeros(num_subframes_simulated,1);              % Number of collisions in each subframe.
+    num_transmissions_list = zeros(num_subframes_simulated,1);  % Number of transmissions in each subframe.
+    
+    % Iterate for each of the simulated subframes
     for s = 1:num_subframes_simulated
-        if mod(s,100000) == 0
-            disp(s)
-        end
         
         % Dynamic scenario: every 10 seconds we randomly change the 
-        % parameters for one of the UEs simulating that a new UE is 
+        % parameters for one of the UEs, simulating that a new UE is 
         % entering into the system (and 1 UE is leaving)
         if (mod(s,10000) == 0)
             rand_UE = randi(num_vehicles);
             UEs(rand_UE).counter = randi([1,C2]);
             UEs(rand_UE).subframe = randi([s,s+RRI-1]);
-            subchannel_start = randi([1,num_subchannels-(packet_subchannels-1)]);
-            UEs(rand_UE).subchannels = subchannel_start:(subchannel_start+packet_subchannels-1);
+            subchannel_start = randi([1,N_subch-(L_subch-1)]);
+            UEs(rand_UE).subchannels = subchannel_start:(subchannel_start+L_subch-1);
         end
         
         % We iterate for each of the simulated vehicles
         for UE = 1:num_vehicles
+            
             % If the current UE transmits in this subframe
             if UEs(UE).subframe == s 
-
+                
                 % If we simulate our proposed approach
                 if proposal == true
                     % Perform counter reselection (if necessary)
@@ -84,14 +71,13 @@ for i = 1:num_simulations
                 
                 % Simulate the current UE's transmission
                 sensing{s} = vertcat(sensing{s},UEs(UE).subchannels(:));
-                
                 num_transmissions_list(s) = num_transmissions_list(s) + 1;
 
-                % If the counter value is not zero don't trigger resource
+                % If the counter value is not zero don't trigger resource 
                 % reselection
                 if UEs(UE).counter ~= 0
-                    UEs(UE).subframe = s + RRI; % Reserve subframe
-                    UEs(UE).counter = UEs(UE).counter - 1; % Update counter
+                    UEs(UE).subframe = s + RRI;             % Reserve subframe
+                    UEs(UE).counter = UEs(UE).counter - 1;  % Update counter
                 % If the counter value is zero, consider triggering 
                 % resource reselection (depending on probResourceKeep)
                 else
@@ -99,13 +85,11 @@ for i = 1:num_simulations
                     % resource reselection.
                     if rand() < probResourceKeep
                         UEs(UE).subframe = s + RRI; % Reserve subframe
-                    
                     % With probability (1-probResourceKeep) trigger
                     % resource reselection
                     else
-                        
                         %%% Sensing-based resource reselection
-                        [subframe, subchannels] = reselection_advanced(s, sensing, T1, T2, RRI, num_subchannels, UEs(UE).subchannels, packet_subchannels);
+                        [subframe, subchannels] = reselection_advanced(s, sensing, T1, T2, RRI, N_subch, UEs(UE).subchannels, L_subch);
                         UEs(UE).subframe = subframe;
                         UEs(UE).subchannels = subchannels;
 
@@ -120,7 +104,7 @@ for i = 1:num_simulations
         end
 
         % Count number of colliding tranmissions in this subframe
-        collisions(s) = calc_collisions(num_subchannels, packet_subchannels, sensing{s});
+        collisions(s) = calc_collisions(N_subch, L_subch, sensing{s});
 
         % If there is a collision in one subframe we delete the counters
         % from the variable that stores them in this subframe so that they

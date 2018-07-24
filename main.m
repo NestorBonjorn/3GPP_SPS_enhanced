514d615cb69860b1c5840007f34a454353a4a32e
NestorBonjorn <nestorbonjorn@gmail.com>
2018-07-24 12:25:23 +0200

    Format

--- main.m
+++ main.m
@@ -1,6 +1,6 @@
 %% Simulation parameters
-proposal = true; % Flag indicating whether we use our proposed approach or not
-num_simulations = 10; % Number of simulations
+proposal = true;                                                    % Flag indicating whether we use our proposed approach or not
+num_simulations = 10;                                               % Number of simulations
 num_subframes_simulated = 1000000;                                  % Number of simulated subframes per simulation
 cumul_cat = zeros(num_subframes_simulated/1000,num_simulations);    % It will store the different simulation results
 
